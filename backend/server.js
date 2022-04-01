d22f2ab24a18a48e484887b8e95d4be4b8ef8c71
Edward Tanguay (hm.win) <edward@tanguay.info>
2022-04-01 15:09:44 +0200

    searchbooks

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,18 @@ const app = express();
 const port = 3055;
 
 app.get('/', (req, res) => {
-	res.send('works');
+	res.send('This is the Gutenberg API. Use /searchbooks');
+});
+
+app.get('/searchbooks', (req, res) => {
+	res.json([
+		{
+			title: "111"
+		},
+		{
+			title: "222"
+		}
+	]);
 });
 
 app.listen(port, () => {

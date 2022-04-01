78c38e1c0ab53412d6554dddcd33286d7c6a642d
Edward Tanguay (hm.win) <edward@tanguay.info>
2022-04-01 15:36:23 +0200

    gets nietzsche books

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import axios from 'axios';
 
 const app = express();
 const port = 3055;
@@ -9,7 +10,16 @@ app.get('/', (req, res) => {
 	res.send('This is the Gutenberg API. Use /searchbooks');
 });
 
+
+
 app.get('/searchbooks', (req, res) => {
+
+	(async () => {
+		const response = await axios.get('https://gutendex.com/books/?search=nietzsche');
+		const rawBooks = response.data;
+		console.log(rawBooks);
+	})();
+
 	res.json([
 		{
 			title: "111"

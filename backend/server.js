f0a98034b565c7c3b0e73b2e1950519f9cab2e76
Edward Tanguay (hm.win) <edward@tanguay.info>
2022-04-01 15:49:55 +0200

    async works

--- backend/server.js
+++ backend/server.js
@@ -10,16 +10,20 @@ app.get('/', (req, res) => {
 	res.send('This is the Gutenberg API. Use /searchbooks');
 });
 
+const getBooks = async () => {
+	const books = [];
+	const response = await axios.get('https://gutendex.com/books/?search=nietzsche');
+	const rawBooks = response.data.results;
+	rawBooks.forEach(rawBook => {
+		books.push({
+			title: rawBook.title
+		});
+	})
+	return books;
+}
 
-
-app.get('/searchbooks', (req, res) => {
-
-	(async () => {
-		const response = await axios.get('https://gutendex.com/books/?search=nietzsche');
-		const rawBooks = response.data;
-		console.log(rawBooks);
-	})();
-
+app.get('/searchbooks', async (req, res) => {
+	const books = await getBooks();
 	res.json([
 		{
 			title: "111"
